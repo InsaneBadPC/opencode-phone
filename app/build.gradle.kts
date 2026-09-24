383e3c1af69738233024b3e4190cf7afc49a5389
InsaneBadPC <petr.lukes0711@gmail.com>
2026-09-24 21:27:05 +0000

    chore(release): bump version to v1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.applet.gnaxcv"
     minSdk = 24
     targetSdk = 36
-    versionCode = 10101
-    versionName = "1.1.1"
+    versionCode = 10200
+    versionName = "1.2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
